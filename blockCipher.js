6e2018bc2e095b7b938bdc891f4c81a09374727a
Kacper Betański <kacper@betanski.dev>
2022-01-10 18:28:58 +0100

    Dokumentacja

--- blockCipher.js
+++ blockCipher.js
@@ -16,6 +16,7 @@ function hexDo8Bit (klucz) {
   return chunkString(klucz, 2).map((hex) => hexDoBin(hex)).join('')
 }
 
+// Przesunięcie w lewo przestawiający najstarszy bit na pozycję najmłodszego
 function przesuńWLewo (str) {
   return str.slice(1, str.length) + str.slice(0, 1)
 }
@@ -30,6 +31,11 @@ function stringXOR (str1, str2, długość) {
   return xor.join('')
 }
 
+// S-Box
+// f1(x1,x2,x3,x4) = x1 ⊕ x1x3 ⊕  x2x4 ⊕x2x3x4 ⊕ x1x2x3x4 ⊕ k1
+// f2(x1,x2,x3,x4) = x2 ⊕ x1x3 ⊕ x1x2x4 ⊕ x1x3x4 ⊕ x1x2x3x4 ⊕ k2
+// f3(x1,x2,x3,x4) = 1 ⊕ x3 ⊕ x1x4 ⊕ x1x2x4 ⊕ x1x2x3x4 ⊕ k3
+// f4(x1,x2,x3,x4) = 1 ⊕ x1x2 ⊕ x3x4 ⊕ x1x2x4 ⊕ x1x3x4 ⊕ x1x2x3x4 ⊕ k4
 const SBox = [
   (x1, x2, x3, x4, k1) => x1 ^ x1 & x3 ^ x2 & x4 ^ x2 & x3 & x4 ^ x1 & x2 & x3 & x4 ^ k1,
   (x1, x2, x3, x4, k2) => x2 ^ x1 & x3 ^ x2 & x4 ^ x1 & x3 & x4 ^ x1 & x2 & x3 & x4 ^ k2,
@@ -37,6 +43,8 @@ const SBox = [
   (x1, x2, x3, x4, k4) => 1 ^ x1 & x2 ^ x3 & x4 ^ x1 & x2 & x4 ^ x1 & x3 & x4 ^ x1 & x2 & x3 & x4 ^ k4
 ]
 
+// Funkcja konwertująca 4 bity danych szyfrowanych oraz klucz rundowy, które są
+// przekazywane do funkcji stanowiących S-Box w odpowiednich typach danych
 function SBoxOut (tekst, kluczRundowy) {
   return SBox[0](parseInt(tekst[0], 2), parseInt(tekst[1], 2), parseInt(tekst[2], 2), parseInt(tekst[4], 2), parseInt(kluczRundowy[0], 2)).toString(2) +
     SBox[1](parseInt(tekst[0], 2), parseInt(tekst[1], 2), parseInt(tekst[2], 2), parseInt(tekst[4], 2), parseInt(kluczRundowy[1], 2)).toString(2) +
@@ -44,12 +52,13 @@ function SBoxOut (tekst, kluczRundowy) {
     SBox[3](parseInt(tekst[0], 2), parseInt(tekst[1], 2), parseInt(tekst[2], 2), parseInt(tekst[4], 2), parseInt(kluczRundowy[3], 2)).toString(2)
 }
 
+// Wybór bitów klucza k1, k3, k5, k7
 function wybor (klucz) {
   return klucz[0] + klucz[2] + klucz[4] + klucz[6]
 }
 
+// Generowanie ośmiu kluczy rundowych
 function generujKluczeRundowe (klucz) {
-  const klucze = []
   const kluczeRundowe = []
 
   let poprzedniKlucz = klucz
@@ -66,13 +75,13 @@ function generujKluczeRundowe (klucz) {
       poprzedniKlucz = przesuńWLewo(poprzedniKlucz)
     }
 
-    klucze.push(poprzedniKlucz)
     kluczeRundowe.push(wybor(poprzedniKlucz))
   }
 
   return kluczeRundowe
 }
 
+// Szyfrowanie przebiega w ośmiu rundach
 function szyfr (tekst, kluczeRundowe) {
   const podzielonyTekst = chunkString(tekst, 4)
 
@@ -102,6 +111,8 @@ function szyfruj (tekst, klucz) {
   return szyfr(tekst, generujKluczeRundowe(klucz))
 }
 
+// Deszyfracja polega na szyfryzacji danych tym samym algorytmem szyfrowanie ale
+// z kluczami podanymi w odwrotnej kolejności. Zgodnie z permutacją Feistela.
 function deszyfruj (tekst, klucz) {
   return szyfr(tekst, generujKluczeRundowe(klucz).reverse())
 }
